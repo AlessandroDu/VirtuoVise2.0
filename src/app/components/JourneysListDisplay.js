225f27ca85d260730e8f40b4bde87e0b13d4cf92
Alessandro <duccialessandro001@gmail.com>
2024-05-10 20:03:15 +0400

    Created the Journeys' listing page design, where users will select their journey

---
+++ src/app/components/JourneysListDisplay.js
@@ -0,0 +1,36 @@
+"use client"
+
+export function JourneysListDisplay() {
+
+
+    return (
+        <div className="journeys-list-page">
+            <div className="journey-display MorningMastery">
+                <div className="text">
+                    <header>Rise and Shine:</header>
+                    <header>Morning Mastery</header>
+                </div>
+            </div>
+            <div className="journey-display NotImplementedYet">
+                <div className="text">
+                    <header>Coming Soon</header>
+                </div>
+            </div>
+            <div className="journey-display NotImplementedYet">
+            <div className="text">
+                    <header>Coming Soon</header>
+                </div>
+            </div>
+            <div className="journey-display NotImplementedYet">
+            <div className="text">
+                    <header>Coming Soon</header>
+                </div>
+            </div>
+            <div className="journey-display NotImplementedYet">
+                <div className="text">
+                    <header>Coming Soon</header>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client"
 
 import Image from "next/image";
+import { JourneysListDisplay } from "./components/JourneysListDisplay";
 
 export default function Home() {
   return (
     <div className="Phone">
       <div className="Screen">
         <div className="Camera"></div>
+        <JourneysListDisplay/>
       </div>
     </div>
   );
